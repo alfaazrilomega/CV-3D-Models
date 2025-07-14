71001cb6a2b96f56ef5caa13af3a3dc15d79d11f
Bernald <143059345+alfaazrilomega@users.noreply.github.com>
2025-07-15 06:45:02 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import LogoSection from "./sections/LogoSection";
 import NavBar from "./components/NavBar";
-import Hero from "./sections/hero";
+import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 import FeaturesCards from "./sections/FeatureCards"
 import ExperienceSection from "./sections/ExperienceSection";
